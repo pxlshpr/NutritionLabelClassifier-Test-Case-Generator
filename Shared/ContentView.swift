828f826ce9994f128334493fe887ae0ff9888e5a
Ahmed Khalaf <pxlshpr@gmail.com>
2022-05-23 21:19:56 +0500

    bump

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -5,15 +5,22 @@ import NutritionLabelClassifier
 import BottomSheet
 import SwiftHaptics
 
+extension Notification.Name {
+    static var resetZoomableScrollViewScale: Notification.Name { return .init("resetZoomableScrollViewScale") }
+}
+
 struct ContentView: View {
 
     @StateObject var vm: ViewModel = ViewModel()
     @State var isPresentingImagePicker = false
     @State var selectedBox: Box? = nil
     
+    @State var isShowingList: Bool = false
+    @State var isHidingBoxes: Bool = false
+
     var body: some View {
         NavigationView {
-            VStack {
+            VStack(spacing: 0) {
                 if let image = vm.pickedImage {
                     GeometryReader { proxy in
                         ZoomableScrollView {
@@ -21,12 +28,37 @@ struct ContentView: View {
                                 .resizable()
                                 .scaledToFit()
                                 .overlay(content: {
-                                    boxesLayer
+                                    if !isHidingBoxes {
+                                        boxesLayer
+                                            .transition(.opacity)
+                                    }
                                 })
                         }
                         .onAppear {
                             vm.contentSize = proxy.size
                         }
+                        .onChange(of: isShowingList) { newValue in
+                            isHidingBoxes = true
+                            NotificationCenter.default.post(name: .resetZoomableScrollViewScale, object: nil)
+                            guard let image = vm.pickedImage else { return }
+                            vm.contentSize = proxy.size
+                            vm.calculateBoxes(in: image)
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                                withAnimation {
+                                    isHidingBoxes = false
+                                }
+                            }
+                        }
+                    }
+                    if isShowingList {
+                        Group {
+                            Divider()
+                            List {
+                                Text("Testing")
+                            }
+                            .listStyle(.plain)
+                        }
+                        .transition(.opacity)
                     }
                 } else {
                     Button("Choose Image") {
@@ -60,6 +92,13 @@ struct ContentView: View {
                 self.isPresentingImagePicker = true
             } label: {
                 Image(systemName: "photo")
+            }
+            Button {
+                withAnimation {
+                    isShowingList.toggle()
+                }
+            } label: {
+                Image(systemName: isShowingList ? "list.bullet.circle.fill" : "list.bullet.circle")
             }
             Spacer()
             Button {
@@ -130,5 +169,59 @@ struct ContentView: View {
             let error = imagePickerError.error
             print("Did Fail with error: \(error) in \(phPickerViewController)")
         })
+    }
+}
+
+import SwiftUI
+
+public struct ZoomableScrollView<Content: View>: UIViewRepresentable {
+    private var content: Content
+    
+    public init(@ViewBuilder content: () -> Content) {
+        self.content = content()
+    }
+    
+    public func makeUIView(context: Context) -> UIScrollView {
+        // set up the UIScrollView
+        let scrollView = UIScrollView()
+        scrollView.delegate = context.coordinator  // for viewForZooming(in:)
+        scrollView.maximumZoomScale = 20
+        scrollView.minimumZoomScale = 1
+        scrollView.bouncesZoom = true
+        
+        // create a UIHostingController to hold our SwiftUI content
+        let hostedView = context.coordinator.hostingController.view!
+        hostedView.translatesAutoresizingMaskIntoConstraints = true
+        hostedView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        hostedView.frame = scrollView.bounds
+        scrollView.addSubview(hostedView)
+        
+        NotificationCenter.default.addObserver(forName: .resetZoomableScrollViewScale, object: nil, queue: .main) { notification in
+            scrollView.setZoomScale(1, animated: true)
+        }
+        return scrollView
+    }
+    
+    public func makeCoordinator() -> Coordinator {
+        return Coordinator(hostingController: UIHostingController(rootView: self.content))
+    }
+    
+    public func updateUIView(_ uiView: UIScrollView, context: Context) {
+        // update the hosting controller's SwiftUI content
+        context.coordinator.hostingController.rootView = self.content
+        assert(context.coordinator.hostingController.view.superview == uiView)
+    }
+    
+    // MARK: - Coordinator
+    public class Coordinator: NSObject, UIScrollViewDelegate {
+        var hostingController: UIHostingController<Content>
+        
+        init(hostingController: UIHostingController<Content>) {
+            self.hostingController = hostingController
+        }
+        
+        public func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+            return hostingController.view
+        }
     }
 }
