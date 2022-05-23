c9394becbcb859937db4b7433cc7650ccf58bf6b
Ahmed Khalaf <pxlshpr@gmail.com>
2022-05-23 11:47:31 +0500

    initial commit

---
+++ Shared/NutritionLabelClassifier_Test_Case_GeneratorApp.swift
@@ -0,0 +1,18 @@
+//
+//  NutritionLabelClassifier_Test_Case_GeneratorApp.swift
+//  Shared
+//
+//  Created by Ahmed Khalaf on 23/5/22.
+//
+
+import SwiftUI
+
+@main
+struct NutritionLabelClassifier_Test_Case_GeneratorApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
